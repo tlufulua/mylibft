62f2345f659e4a5c7a24e1072eb1583179bab201
Teresa Bulungu Lufuluabo Pastor <teresalufu@MacBook-Pro-de-Teresa.local>
2021-09-01 23:44:01 +0200

    mylibft v.2: ft_strchr

--- srcs/ft_strchr.c
+++ srcs/ft_strchr.c
@@ -16,9 +16,11 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 	{
-		if (*s == (char)c || c == '\0')
+		if (*s == c)
 			return ((char *)s);
 		s++;
+		if (*s == '\0' && c == '\0')
+			return ((char *)s);
 	}
 	return (0);
 }
