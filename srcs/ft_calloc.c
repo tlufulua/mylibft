127681539191d9c25d1bb3ab1e76a7bd0ebcfdae
Teresa bulungu Lufuluabo pastor <tlufulua@student.42madrid.com>
2022-08-01 20:47:00 +0200

    better ft_calloc

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,11 +6,10 @@
 /*   By: tlufulua <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 15:57:31 by tlufulua          #+#    #+#             */
-/*   Updated: 2022/08/01 20:40:16 by tlufulua         ###   ########.fr       */
+/*   Updated: 2022/08/01 20:46:25 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <libft.h>
 
 void	*ft_calloc(unsigned int count, unsigned int size)
